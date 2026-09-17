747b0aa1f5e7968892e17994886daf780b9e5745
Stepan <Stepanmantrov12.05@gmail.com>
2026-09-17 13:41:39 +0300

    fix(): Фикс шрифтов для адаптива

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./styles.module.scss";
+
+import "./styles.scss";
+
 import { PatientsListPage } from "@/pages/PatientsListPage";
 import { AnamnesisPage } from "@/pages/AnamnesisPage";
 import { PatientFormPage } from "@/pages/PatientFormPage";
@@ -10,17 +12,16 @@ export const App: React.FC = () => {
     <BrowserRouter basename="/Endo-anamnesis">
       <Routes>
         <Route path="/" element={<PatientsListPage />} />
-        <Route path="/patient/new" element={<PatientFormPage />} />{" "}
-        {/* если есть */}
-        <Route
-          path="/patient/:patientId/edit"
-          element={<PatientFormPage />}
-        />{" "}
-        {/* если есть */}
+
+        <Route path="/patient/new" element={<PatientFormPage />} />
+
+        <Route path="/patient/:patientId/edit" element={<PatientFormPage />} />
+
         <Route
           path="/patient/:patientId/anamnesis"
           element={<AnamnesisPage />}
         />
+
         <Route
           path="/patient/:patientId/anamnesis/:anamnesisId"
           element={<AnamnesisPage />}
